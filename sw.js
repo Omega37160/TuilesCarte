a0ced3e38373aa120f66c849cf73839dd6b3eba8
Omega37160 <159494974+Omega37160@users.noreply.github.com>
2026-05-07 11:43:56 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
 const CACHE_NAME = 'offline-maps-v1';
 
 self.addEventListener('fetch', event => {
-    const url = event.request.url;
-
-    // Intercepte les tuiles locales
-    if (url.includes('/local-tiles/')) {
+    if (event.request.url.includes('/local-tiles/')) {
         event.respondWith(
             caches.match(event.request)
-                .then(response => {
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                })
+                .then(cached => cached || fetch(event.request))
         );
     }
 });
